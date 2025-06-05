1d5857b7ec7520b855728e83c5a93fb879d0ef3c
lbarreto-tech <lucaslbcs2003@gmail.com>
2025-06-04 21:19:21 -0300

    Corrigido

--- src/main/java/TesteDao.java
+++ src/main/java/TesteDao.java
@@ -11,18 +11,8 @@ import model.CargoModel;
 public class TesteDao {
 	public static void main(String[] args) throws ParseException {
 		
-		CargoModel cargo = new CargoModel();
-		cargo.setNomeCargo("GESTOR");
-		
 		CargoDao cargoDao = new CargoDao();
-		cargoDao.salvar(cargo);
-		
-		List<CargoModel> list = cargoDao.listarTodos();
-		for (CargoModel cargoModel : list) {
-			System.out.println(cargoModel);
-			System.out.println();
-		}
-	
+		System.out.println(cargoDao.verificaCargo("ana@empresa.com", "senha123"));
     
 	}
 }
--- src/main/java/dao/CargoDao.java
+++ src/main/java/dao/CargoDao.java
@@ -93,12 +93,14 @@ public class CargoDao {
 		String sql = "select c.nomeCargo from email e ";
 			   sql+= " join usuario u on e.idUsuarioFk = u.idUsuario ";
 			   sql+= " join cargo c on u.idCargoFk = c.idCargo ";
-			   sql+= "where e.enderecoEmail = " + email + " and e.senha = " + password;
+			   sql+= "where e.enderecoEmail = ? and e.senha = ?";
 			   
 			   try {
 				   PreparedStatement statement = connection.prepareStatement(sql);
 					ResultSet resultado = statement.executeQuery();
-					
+					statement.setString(1, email);
+					statement.setString(2, password);
+
 					while(resultado.next()) {
 						model.setNomeCargo(resultado.getString("nomeCargo"));
 						retorno = model.getNomeCargo();
